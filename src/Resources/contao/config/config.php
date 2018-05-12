213f04655aca66fbf47e02a1dad95de3998738cc
Alexander Urban <alexander.urban@cepharum.de>
2018-05-12 13:42:12 +0200

    WIP: Setup back end module

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,29 +2,18 @@
 
 //$GLOBALS['TL_HOOKS']['processFormData'][] =['mybundle.listener.process_form_data', 'onProcessFormData'];
 
-/**
- * Add back end modules:
- */
+// Add back end modules
 array_insert($GLOBALS['BE_MOD'], 1, [
 	'CepharumTimetable' => [
-		'Sites and rooms' => [
-			'tables' 	=> ['tl_timetable_sites', 'tl_timetable_rooms'],
-			'icon'   	=> 'home.gif',
-		],
-		'Teachers' => [
-			'tables'	=> ['tl_timetable_teachers'],
-			'icon'		=> 'home.gif',
-		],
-		'Dancestyles'	=> [
-			'tables'	=> ['tl_timetable_styles'],
-			'icon'		=> 'home.gif',
-		],
+		'Sites and rooms'	=> ['tables' => ['tl_timetable_sites', 'tl_timetable_rooms'],	'icon' => 'home.gif'],
+		'Teachers'			=> ['tables' => ['tl_timetable_teachers'],						'icon' => 'home.gif'],
+		'Dancestyles'		=> ['tables' => ['tl_timetable_styles'],						'icon' => 'home.gif'],
 	]
 ]);
 
-/**
- * Add front end module:
- */
-if (! array_key_exists('Cepharum', $GLOBALS['FE_MOD']))
-		$GLOBALS['FE_MOD']['Cepharum'] = array();
-$GLOBALS['FE_MOD']['Cepharum']['Timetable'] = 'Cepharum\\TimetableBundle\\Module\\TimetableView';
+// Front end module(s):
+array_insert($GLOBALS['FE_MOD'], 3, ['Cepharum' => ['timetableview' => 'ModuleTimetableView']]);
+
+// if (! array_key_exists('Cepharum', $GLOBALS['FE_MOD']))
+// 		$GLOBALS['FE_MOD']['Cepharum'] = array();
+// $GLOBALS['FE_MOD']['Cepharum']['Timetable'] = 'Cepharum\\TimetableBundle\\Module\\TimetableView';
